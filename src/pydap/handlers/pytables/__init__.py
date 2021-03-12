c94ebddec340f1b493ae12e7d4ec4e62264da551
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2021-03-11 16:12:11 -0800

    Updated handler, requires developmental version of pytables for column attributes

--- src/pydap/handlers/pytables/__init__.py
+++ src/pydap/handlers/pytables/__init__.py
@@ -1,6 +1,4 @@
-"""Pydap handler for HDF5 files using h5py."""
-
-# TODO: add attributes
+"""Pydoop handler for HDF5 files using h5py."""
 
 import os
 import re
@@ -15,12 +13,15 @@ from pydap.model import *
 from pydap.handlers.lib import BaseHandler
 from pydap.exceptions import OpenFileError
 
+import re
+_col_match_re = re.compile('FIELD_\d+_ATTR_')
+
 
 class HDF5Handler(BaseHandler):
 
     """A simple handler for HDF5 files based on PyTables."""
 
-    #__version__ = get_distribution("pydap.handlers.hdf5").version
+    __version__ = get_distribution("pydap.handlers.pytables").version
     extensions = re.compile(r"^.*\.(h5|hdf5)$", re.IGNORECASE)
 
     def __init__(self, filepath):
@@ -40,25 +41,39 @@ class HDF5Handler(BaseHandler):
 
         # build dataset
         name = os.path.split(filepath)[1]
+        node = self.fp.root
         self.dataset = DatasetType(name, attributes={
-            "NC_GLOBAL": {},
+            "NC_GLOBAL": get_attrs(node),
         })
         build_dataset(self.dataset, self.fp, self.fp.root)
+        self.fp.close()
 
+def get_attrs(node):
+    attrs = {k: node._v_attrs[k] for k in node._v_attrs._v_attrnames
+        if k.upper() != k
+    }
+    return attrs
 
 def build_dataset(target, fp, node):
     """Recursively build a dataset, mapping groups to structures."""
     for node in fp.list_nodes(node):
         if isinstance(node, tables.Group):
-            target[node._v_name] = StructureType(node._v_name)
-            build_dataset(target[node._v_name], fp, node)
+            attrs = {k: node._v_attrs[k] for k in node._v_attrs._v_attrnames}
+            target[node._v_name] = StructureType(node._v_name, attributes=get_attrs(node)
+                    )
+            build_dataset(target[node._v_name], fp, node,)
         elif isinstance(node, tables.Array):
             target[node._v_name] = BaseType(
-                node._v_name, node, None)
+                node._v_name, node, None, attributes=get_attrs(node))
+
         elif isinstance(node, tables.Table):
-            sequence = target[node._v_name] = SequenceType(node._v_name)
-            for name in node.colnames:
-                sequence[name] = BaseType(name, node.coldtypes[name])
+            table = node
+            table_attrs = {k: v for k, v in get_attrs(node).items() if not _col_match_re.match(k)}
+            sequence = target[node._v_name] = SequenceType(node._v_name, attributes=table_attrs)
+
+            for name in table.colnames:
+                col_attrs = dict(getattr(table.cols, name).attrs)
+                sequence[name] = BaseType(name, table.coldtypes[name], attributes=col_attrs)
             sequence.data = node.read()
 
 
